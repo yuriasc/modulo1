e695103c3fd52671a2a99077094158df9d381d67
Yuri Canuto <yuriasc@gmail.com>
2019-08-08 21:42:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const app = express()
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
+
+const users = ['Yuri Canuto', 'Priscila Canuto', 'Leon Canuto']
+
+app.get('/', (req, res) => {
+  return res.render('list', { users })
+})
+
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+  res.redirect('/')
+})
+
+app.listen(3000)
